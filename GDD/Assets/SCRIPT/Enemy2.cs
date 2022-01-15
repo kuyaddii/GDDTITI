08adb0fd5f975c57b8df066fc35bd3b7be466449
Aulkanic <michaeltrazona9@gmail.com>
2022-01-15 01:16:08 -0800

    aniamtion

--- GDD/Assets/SCRIPT/Enemy2.cs
+++ GDD/Assets/SCRIPT/Enemy2.cs
@@ -2,46 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy2 : MonoBehaviour
 {
 
-	public int health = 500;
+	public int health = 1000;
 	public Animator animator;
 
 	private void Start()
 	{
-		
+
 		animator = GetComponent<Animator>();
 		//DontDestroyOnLoad (gameObject);
 	}
 
 	public GameObject deathEffect;
-	public GameObject RageEffect;
 
 
 	public void TakeDamage(int damage)
 	{
 		health -= damage;
+
 		if (health <= 550)
 		{
 			animator.SetTrigger("MATTCK");
 
 		}
-
-		if (health <= 300)
-		{
-			animator.SetTrigger("Attack");
-			
-		}
-		if (health <= 200)
-		{
-			Rage();
-		}
-		//if (health <= 25)
-		///{
-		//	animator.SetTrigger("rage"); ;
-		//}
-
+		
 		if (health <= 0)
 		{
 			Die();
@@ -51,11 +37,6 @@ public class Enemy : MonoBehaviour
 	void Die()
 	{
 		Instantiate(deathEffect, transform.position, Quaternion.identity);
-		Destroy(gameObject);
-	}
-	void Rage()
-	{
-		Instantiate(RageEffect, transform.position, Quaternion.identity);
 		Destroy(gameObject);
 	}
 
