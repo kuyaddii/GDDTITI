f71905e88e946a34b1ebd2743edf373e5550b632
Aulkanic <michaeltrazona9@gmail.com>
2022-01-14 17:43:13 -0800

    animation

--- GDD/Assets/SCRIPT/HealthBar1.cs
+++ GDD/Assets/SCRIPT/HealthBar1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthBar : MonoBehaviour
+public class HealthBar1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
